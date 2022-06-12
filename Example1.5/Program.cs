586b8604a50be23b78dbd1f0c034ad2db089c4b7
Aleksey <aleksey-asapov@mail.ru>
2022-06-12 10:58:01 +0300

    In the example1.5 dismantled IF and ELSE

---
+++ Example1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя польхователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
