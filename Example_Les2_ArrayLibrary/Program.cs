7d68acc0df1adeca76c0f3f490b5ed09b9cf6116
Aleksey <aleksey-asapov@mail.ru>
2022-06-13 23:00:19 +0300

    Lesson 2 Функции и массивы

---
+++ Example_Les2_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿void FillArray(int[] collection) // даем наименование методу collection - наименование
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // новое случайно целое  число
+        //index = index + 1;
+        index++;
+    }
+}
+// void - это метод ничего не возвращает (return)
+void PrintArray(int[] col) // Метод void который будет печатать массив, col - новый аргумент
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // new .. Создай новый массив в котором будет 10 эллементов
+
+FillArray(array); // Передадим наименование нашего массива
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); // Печатаем наш масиив
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
+
+
+
---
+++ Example_Les2_MetodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 19, 16, 19, 17 };
+
+int n = array.Length;
+int find = 19;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) // равен элементу find
+    {
+        Console.WriteLine(index);
+        break; // если 2 числа одинаковых
+    }
+    //index = index + 1;
+    index++;
+}
